294082885e47a85d5d415d20f205b017f48dd5ab
周振源 <20373427@buaa.edu.cn>
2023-05-25 00:08:28 +0800

    实现复向量与整系数多项式的编解码

--- include/Encoder.h
+++ include/Encoder.h
@@ -4,19 +4,24 @@
 
 #ifndef CKKS_DEMO_ENCODER_H
 #define CKKS_DEMO_ENCODER_H
+#include <utility>
+
 #include "complex"
 #include "vector"
 #include "Polynomial.h"
 #include "iostream"
 #include "Eigen/Dense"
+#include "random"
 
 class Encoder {
 private:
     int m;
+    int scale;
 
 public:
-    explicit Encoder(int m) {
+    explicit Encoder(int m, int scale) {
         this->m = m;
+        this->scale = scale;
     }
 
     auto get_unit_root() const {
@@ -67,6 +72,113 @@ public:
             poly.add_term(i, eigen_res(i));
         }
         return poly;
+    }
+
+    auto pi(std::vector<std::complex<double>> vec) const {
+        auto n = m / 4;
+        std::vector<std::complex<double>> new_vec;
+        for(int i = 0; i < n; ++i) {
+            new_vec.push_back(vec.at(i));
+        }
+        return new_vec;
+    }
+
+    auto pi_inverse(std::vector<std::complex<double>> vec) const {
+        auto n = m / 4;
+        std::vector<std::complex<double>> new_vec;
+        for(int i = 0; i < n; ++i) {
+            new_vec.push_back(vec.at(i));
+        }
+        for(int i = n - 1; i >= 0; --i) {
+            new_vec.push_back(conj(vec.at(i)));
+        }
+        return new_vec;
+    }
+
+    auto get_sigma_R_basis() const {
+        auto n = m / 2;
+        auto matrix = get_vandermonde();
+        std::vector<std::vector<std::complex<double>>> basis;
+        for(int i = 0; i < n; ++i) {
+            std::vector<std::complex<double>> vec(n);
+            for(int j = 0; j < n; ++j) {
+                vec.at(j) = matrix.at(j).at(i);
+            }
+            basis.push_back(vec);
+        }
+        return basis;
+    }
+
+    auto vdot(std::vector<std::complex<double>> a, std::vector<std::complex<double>> b) {
+        assert(a.size() == b.size());
+        std::complex<double> complex = 0;
+        for(int i = 0; i < a.size(); ++i) {
+            complex += conj(a.at(i)) * b.at(i);
+        }
+        return complex.real();
+    }
+
+    auto get_basis_coordinates(std::vector<std::complex<double>> vec) {
+        std::vector<double> new_vec;
+        auto basis = get_sigma_R_basis();
+        for(auto i : basis) {
+            new_vec.push_back(vdot(vec, i) / vdot(i, i));
+        }
+        return new_vec;
+    }
+
+    auto coordinate_wise_random_rounding(std::vector<double> vec) {
+        std::vector<int> new_vec;
+        std::random_device rd;
+        auto gen = std::default_random_engine(rd());
+        std::uniform_int_distribution<int> dist(1,100);
+        for(auto i : vec) {
+            if(dist(gen) % 2) {
+                new_vec.push_back(std::floor(i));
+            } else {
+                new_vec.push_back(std::ceil(i));
+            }
+        }
+        return new_vec;
+    }
+
+    auto sigma_R_discrete(std::vector<std::complex<double>> z) {
+        auto coordinates = get_basis_coordinates(z);
+        auto rounded_coordinates = coordinate_wise_random_rounding(coordinates);
+        auto matrix = get_vandermonde();
+        std::vector<std::complex<double>> vec;
+        for(auto & i : matrix) {
+            std::complex<double> complex = 0;
+            for(int j = 0; j < rounded_coordinates.size(); j++) {
+                complex += std::complex<double>{i.at(j).real() * rounded_coordinates.at(j), i.at(j).imag() * rounded_coordinates.at(j)};
+            }
+            vec.emplace_back(complex);
+        }
+        return vec;
+    }
+
+    auto encode(std::vector<std::complex<double>> z) {
+        auto pi_z = pi_inverse(std::move(z));
+        std::vector<std::complex<double>> scaled_pi_z;
+        for(auto i : pi_z) {
+            scaled_pi_z.emplace_back(i.real() * scale, i.imag() * scale);
+        }
+        auto rounded_scaled_pi_z = sigma_R_discrete(scaled_pi_z);
+        auto poly = sigma_inverse(rounded_scaled_pi_z);
+        Polynomial new_poly;
+        for(auto term : poly.get_terms()) {
+            new_poly.add_term(term.first, int(term.second.real()));
+        }
+        return new_poly;
+    }
+
+    auto decode(Polynomial poly) {
+        Polynomial new_poly;
+        for(auto term : poly.get_terms()) {
+            new_poly.add_term(term.first, {term.second.real() / scale, term.second.imag() / scale});
+        }
+        auto vec = sigma(new_poly);
+        return pi(vec);
     }
 };
 #endif //CKKS_DEMO_ENCODER_H
--- src/encoder_test.cpp
+++ src/encoder_test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "../include/Encoder.h"
 
-auto encoder = Encoder(8);
+auto encoder = Encoder(8, 64);
 
 void basic_test() {
     std::cout << "basic test" << std::endl;
@@ -9,6 +9,13 @@ void basic_test() {
     std::cout << encoder.get_power_of_unit_root(4) << std::endl;
     auto m = encoder.get_vandermonde();
     for(auto i : m) {
+        for(auto j : i) {
+            std::cout << j << " ";
+        }
+        std::cout << std::endl;
+    }
+    auto basis = encoder.get_sigma_R_basis();
+    for(const auto& i : basis) {
         for(auto j : i) {
             std::cout << j << " ";
         }
@@ -43,9 +50,31 @@ void mult_test() {
     }
 }
 
+void encode_test() {
+    std::cout << "encode test" << std::endl;
+    std::vector<std::complex<double>> vec = {{3, 4}, {2, -1}};
+    auto poly = encoder.encode(vec);
+    std::cout << poly << std::endl;
+}
+
+void decode_test() {
+    std::cout << "decode test" << std::endl;
+    Polynomial poly;
+    poly.add_term(0, 160);
+    poly.add_term(1, 90);
+    poly.add_term(2, 160);
+    poly.add_term(3, 45);
+    auto vec = encoder.decode(poly);
+    for(auto i : vec) {
+        std::cout << i << std::endl;
+    }
+}
+
 int main() {
     basic_test();
     add_test();
     mult_test();
+    encode_test();
+    decode_test();
     return 0;
 }
